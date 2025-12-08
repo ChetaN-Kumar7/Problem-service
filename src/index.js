a07b5158c31c8e8b7fe58c799d9ff6e01c057cbe
ChetaN-Kumar7 <chetankumar3018@gmail.com>
2025-12-08 10:48:31 +0530

    Feat : Added routing structure for versioned api for problems

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser=require('body-parser')
 const{PORT} = require('./config/server.config')
+const apiRouter = require('./routes')
 
 const app= express()
 
@@ -8,6 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.text());
 
+app.use('/api',apiRouter)
 
 app.get('/ping',(req,res)=>{
     return res.json({message:"Jais shree ram"})
